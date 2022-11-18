d351d9b125853218f5a6b41f506df5809a89870e
Nika Skhulukhia <nskhu12@gmail.com>
2022-11-18 14:00:43 +0400

    initial commit

---
+++ src/Battery.java
@@ -0,0 +1,15 @@
+public class Battery {
+    private String brand;
+    private String type;
+    private int volume;
+
+    public Battery(String brand, String type, int volume) {
+        this.brand = brand;
+        this.type = type;
+        this.volume = volume;
+    }
+
+    public Battery(){
+
+    }
+}
