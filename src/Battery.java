d94a66073a00ce9ef6b61f710a185f3cb84ea53a
Nika Skhulukhia <nskhu12@gmail.com>
2022-11-18 14:59:55 +0400

    init 2

--- src/Battery.java
+++ src/Battery.java
@@ -1,15 +1,57 @@
 public class Battery {
     private String brand;
     private String type;
-    private int volume;
+    private int capacity;
+    private int life;
 
-    public Battery(String brand, String type, int volume) {
+    public Battery(String brand, String type, int capacity, int life) {
         this.brand = brand;
         this.type = type;
-        this.volume = volume;
+        this.capacity = capacity;
+        this.life = life;
     }
 
     public Battery(){
 
+    }
+
+    @Override
+    public String toString() {
+        String result = "This is " + this.type + " battery, with volume of "
+                + this.capacity + " mAH, made by " + this.brand + ".";
+        result += " Currently it is charged for " + this.life + "%.";
+        return result;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Battery lion1 = new Battery();
+        lion1.setBrand("iPhone");
+        lion1.setType("nonremovable");
+        lion1.setCapacity(3000);
+        lion1.setLife(35);
+        System.out.println(lion1.toString());
     }
 }
