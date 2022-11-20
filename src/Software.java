d0d4c412fd4ad36e357971248ec443d2e7914653
Nika Skhulukhia <nskhu12@gmail.com>
2022-11-20 21:16:07 +0400

    implement software class

--- src/Software.java
+++ src/Software.java
@@ -1,2 +1,56 @@
-package PACKAGE_NAME;public class Software {
+import java.util.IllformedLocaleException;
+
+public class Software {
+    private String type;
+    Private int version; // greater or equal to zero, default 0;
+
+    public Software(String type, int version) {
+        if (version < 0) {
+            throw new IllegalArgumentException("Version must be greater or equal to zero!");
+        } else {
+            this.type = type;
+            this.version = version;
+        }
+    }
+
+    public Software() {
+    }
+
+    // Reset software version to zero
+    public void Reset() {
+        this.version = 0;
+    }
+
+    // increase software version by one
+    public void Update() {
+        this.version++;
+    }
+
+    @Override
+    public String toString() {
+        return "Software{" +
+                "type='" + type + '\'' +
+                ", version=" + version +
+                '}';
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        if (version < 0) {
+            throw new IllegalArgumentException("Version must be greater or equal to zero!");
+        } else {
+            this.version = version;
+        }
+    }
 }
