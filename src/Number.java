304b7a022b45a534c9393013111a188563ad5c99
Nika Skhulukhia <nskhu12@gmail.com>
2022-11-20 18:45:10 +0400

    implement number class

--- src/Number.java
+++ src/Number.java
@@ -1,2 +1,70 @@
-package PACKAGE_NAME;public class Number {
+public class Number {
+    private static final String preIndex = "+"; // constant symbol '+'
+    private String brand;
+    private String index;
+    private String number;
+    private Person owner;
+    private Phone phone;
+
+    public Number(String brand, String index, String number, Person owner, Phone phone) {
+        this.brand = brand;
+        this.index = index;
+        this.number = number;
+        this.owner = owner;
+        this.phone = phone;
+    }
+
+    public Number() {
+    }
+
+    @Override
+    public String toString() {
+        return "Number{" +
+                "brand='" + brand + '\'' +
+                ", index='" + index + '\'' +
+                ", number='" + number + '\'' +
+                ", owner=" + owner +
+                ", phone=" + phone +
+                '}';
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getIndex() {
+        return index;
+    }
+
+    public void setIndex(String index) {
+        this.index = index;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
+
+    public Phone getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Phone phone) {
+        this.phone = phone;
+    }
 }
---
+++ src/Phone.java
@@ -0,0 +1,11 @@
+public class Phone implements Gadget{
+    @Override
+    public void Charge(int time) {
+
+    }
+
+    @Override
+    public void ChangeBattery() {
+
+    }
+}
